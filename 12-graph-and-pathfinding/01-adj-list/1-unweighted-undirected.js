1795ba4920362c9e92078ba19fba46a526355b16
Hrittik Bhattacharjee <impulsiv.rekt.mind@gmail.com>
2021-10-10 16:21:53 +0530

    update weighted undirected

--- 12-graph-and-pathfinding/01-adj-list/1-unweighted-undirected.js
+++ 12-graph-and-pathfinding/01-adj-list/1-unweighted-undirected.js
@@ -48,6 +48,7 @@ class Graph {
             const adjacentVertex = this.adjacencyList[vertex].pop();
             this.removeEdge(vertex, adjacentVertex);
         }
+
         delete this.adjacencyList[vertex];
     }
 
--- 12-graph-and-pathfinding/01-adj-list/2-weighted-undirected.js
+++ 12-graph-and-pathfinding/01-adj-list/2-weighted-undirected.js
@@ -15,6 +15,22 @@ class Graph {
         this.adjacencyList[v1].push({ vertex: v2, weight });
         this.adjacencyList[v2].push({ vertex: v1, weight });
     }
+
+    // removeEdge: Removing an edge between two vertices in the adjacency list
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(item => item.vertex !== v2);
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(item => item.vertex !== v1);
+    }
+
+    // removeVertex: Removing a vertex from the adjacency list
+    removeVertex(vertex) {
+        while (this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex.vertex);
+        }
+
+        delete this.adjacencyList[vertex];
+    }
 }
 
 module.exports = Graph;
